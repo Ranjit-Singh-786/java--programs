53f010e952939f8ac3b1788491cd052d6b23027e
Ranjit-Singh-786 <jiradhey402@gmail.com>
2021-07-24 00:18:17 +0530

    loop file

---
+++ loopp.java
@@ -0,0 +1,12 @@
+class Loopp{
+public static void main (String [] ar){
+// while looop syntex
+//int x =1;
+//while(x<=5){
+//System.out.println(x);
+//x++;
+//for loop syntex
+for (int i=0; i<=10;i++){
+System.out.println(i);
+
+}}}
